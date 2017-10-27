59fc42c201471bdcd17965a976ccbce4f7a5eaaa
Stan <iamjacke@gmail.com>
2017-10-27 07:29:35 +0500

    contracts

---
+++ login/ethereum-login-sample-master/solidity/truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+      from: '0x6b0c034123f01ff7e48801ba7d32cb3d7d677ca3'
+    }
+  }
+};
