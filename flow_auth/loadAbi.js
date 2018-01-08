1fece335dc5bd45d937e7368b1cd55cf3afc384a
Stan <iamjacke@gmail.com>
2018-01-08 10:37:18 +0300

    mintable contracts

--- flow_auth/loadAbi.js
+++ flow_auth/loadAbi.js
@@ -7,3 +7,25 @@ var web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 var MyContract = web3.eth.contract(json.abi);
 var MyContractInstance = MyContract.at('0x56a121a308490b015ee881794f5c4a570684d275');
 
+
+const IPFS = require('ipfs')
+
+const node = new IPFS()
+
+node.on('ready', () => {
+  // Your node is now ready to use \o/
+  console.log('node ready')
+  
+
+node.files.cat('Qme75koLQzAbF6feCCAK8Tgza923fvqJSUEw2xqF3AkzPH', function (err, file) {
+  if (err) {
+    console.log(err)
+    throw err
+  }
+  console.log(file);
+  console.log(file.toString('utf8'))
+})	
+
+
+// stopping a node
+})
--- flow_manage/contracts/SpreadToken.sol
+++ flow_manage/contracts/SpreadToken.sol
@@ -1,14 +1,15 @@
 pragma solidity ^0.4.17;
 
 import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+import 'zeppelin-solidity/contracts/token/CappedToken.sol';
+import 'zeppelin-solidity/contracts/token/BurnableToken.sol';
 
-contract SpreadToken is StandardToken {
+contract SpreadToken is MintableToken, BurnableToken {
 
 string public name = 'SpreadToken';
 string public symbol = 'SPRED';
 uint8 public decimals = 2;
 uint public INITIAL_SUPPLY = 12000;
-
 
 function SpreadToken() public {
   totalSupply = INITIAL_SUPPLY;
